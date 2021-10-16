2985e2b69da32eddb0cf74afa848bfda770c5ae6
AestheticalZ <51166756+AestheticalZ@users.noreply.github.com>
2021-10-16 19:02:19 +0200

    Admin Module improvements.

--- Modules/AdminModule.cs
+++ Modules/AdminModule.cs
@@ -31,8 +31,14 @@ namespace SammBotNET.Modules
             if (Context.Message.Author.Id != GlobalConfig.Instance.LoadedConfig.AestheticalUid)
                 return ExecutionResult.FromError("You are not allowed to execute this command.");
 
+            if (Context.Client.GetGuild(guild) == null) return ExecutionResult.FromError("I am not invited in that guild!");
+            if (Context.Client.GetGuild(guild).GetTextChannel(channel) == null)
+                return ExecutionResult.FromError($"Channel with ID {channel} does not exist in guild with ID {guild}.");
+
             AdminService.ChannelId = channel;
             AdminService.GuildId = guild;
+
+            await ReplyAsync($"Success. Set guild to `{guild}` and channel to `{channel}`.");
 
             return ExecutionResult.Succesful();
         }
