89ff4a556cb080402e04071c91869979917e0b18
AestheticalZ <51166756+AestheticalZ@users.noreply.github.com>
2021-10-17 14:41:51 +0200

    Testing?

--- Modules/HelpModule.cs
+++ Modules/HelpModule.cs
@@ -22,71 +22,72 @@ namespace SammBotNET.Modules
         [Command("help")]
         [HideInHelp]
         [Summary("Provides all commands and modules available.")]
-        public async Task<RuntimeResult> HelpAsync([Remainder] string CommandName = null)
+        public async Task<RuntimeResult> HelpAsync()
         {
-            if (CommandName == null)
+            EmbedBuilder embed = new()
             {
-                EmbedBuilder embed = new()
-                {
-                    Color = Color.DarkPurple,
-                    Title = "SAMM-BOT HELP",
-                    Description = $"These are all of the modules available." +
-                    $"\n Use `s.help <Module Name>` to see its commands."
-                };
+                Color = Color.DarkPurple,
+                Title = "SAMM-BOT HELP",
+                Description = $"These are all of the modules available." +
+                $"\n Use `s.help <Module Name>` to see its commands."
+            };
 
-                foreach (ModuleInfo module in CommandService.Modules)
-                {
-                    bool foundCommand = false;
-
-                    foreach (CommandInfo cmd in module.Commands)
-                    {
-                        PreconditionResult result = await cmd.CheckPreconditionsAsync(Context);
-
-                        if (cmd.Attributes.Any(x => x is HideInHelp)) continue;
-
-                        if (result.IsSuccess) foundCommand = true;
-                    }
-
-                    if(foundCommand)
-                        embed.AddField(module.Name, string.IsNullOrEmpty(module.Summary) ? "No description." : $"`{module.Summary}`", true);
-                }
-                embed.WithAuthor(author => author.Name = "SAMM-BOT COMMANDS");
-                embed.WithFooter(footer => footer.Text = "Samm-Bot");
-                embed.WithCurrentTimestamp();
-
-                await ReplyAsync("", false, embed.Build());
-            }
-            else
+            foreach (ModuleInfo module in CommandService.Modules)
             {
-                SearchResult result = CommandService.Search(Context, CommandName);
+                bool foundCommand = false;
 
-                if (!result.IsSuccess)
-                    return ExecutionResult.FromError($"The command \"{CommandName}\" doesn't exist.");
-
-                EmbedBuilder embed = new()
+                foreach (CommandInfo cmd in module.Commands)
                 {
-                    Title = "SAMM-BOT HELP",
-                    Description = $"All commands that are called \"{CommandName}\":",
-                    Color = Color.DarkPurple,
-                };
+                    PreconditionResult result = await cmd.CheckPreconditionsAsync(Context);
 
-                foreach (CommandMatch match in result.Commands)
-                {
-                    CommandInfo cmd = match.Command;
+                    if (cmd.Attributes.Any(x => x is HideInHelp)) continue;
 
-                    string parameters = string.Join(", ", cmd.Parameters.Select(p => p.Name));
-                    if (parameters == string.Empty) parameters = "no parameters.";
-
-                    string description = cmd.Summary ?? (description = "No description provided.");
-
-                    embed.AddField(string.Join(", ", cmd.Aliases), $"Parameters: {parameters}\nDescription: {cmd.Summary}", false);
+                    if (result.IsSuccess) foundCommand = true;
                 }
-                embed.WithAuthor(author => author.Name = "SAMM-BOT COMMANDS");
-                embed.WithFooter(footer => footer.Text = "Samm-Bot");
-                embed.WithCurrentTimestamp();
 
-                await ReplyAsync("", false, embed.Build());
+                if (foundCommand)
+                    embed.AddField(module.Name, string.IsNullOrEmpty(module.Summary) ? "No description." : $"`{module.Summary}`", true);
             }
+            embed.WithAuthor(author => author.Name = "SAMM-BOT COMMANDS");
+            embed.WithFooter(footer => footer.Text = "Samm-Bot");
+            embed.WithCurrentTimestamp();
+
+            await ReplyAsync("", false, embed.Build());
+        }
+
+        [Command("help")]
+        [HideInHelp]
+        [Summary("Provides all commands and modules available.")]
+        public async Task<RuntimeResult> HelpAsync([Remainder] string moduleName)
+        {
+            SearchResult result = CommandService.Search(Context, moduleName);
+
+            if (!result.IsSuccess)
+                return ExecutionResult.FromError($"The command \"{moduleName}\" doesn't exist.");
+
+            EmbedBuilder embed = new()
+            {
+                Title = "SAMM-BOT HELP",
+                Description = $"All commands that are called \"{moduleName}\":",
+                Color = Color.DarkPurple,
+            };
+
+            foreach (CommandMatch match in result.Commands)
+            {
+                CommandInfo cmd = match.Command;
+
+                string parameters = string.Join(", ", cmd.Parameters.Select(p => p.Name));
+                if (parameters == string.Empty) parameters = "no parameters.";
+
+                string description = cmd.Summary ?? (description = "No description provided.");
+
+                embed.AddField(string.Join(", ", cmd.Aliases), $"Parameters: {parameters}\nDescription: {cmd.Summary}", false);
+            }
+            embed.WithAuthor(author => author.Name = "SAMM-BOT COMMANDS");
+            embed.WithFooter(footer => footer.Text = "Samm-Bot");
+            embed.WithCurrentTimestamp();
+
+            await ReplyAsync("", false, embed.Build());
 
             return ExecutionResult.Succesful();
         }
