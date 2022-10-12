68cc307a636c0bd19a59e07406ec764513b8cc74
AestheticalZ <51166756+AestheticalZ@users.noreply.github.com>
2022-10-12 21:32:43 +0200

    [Bot -> Database, Migrations] Update GuildConfig, add LoggingWelcome migration.

--- SammBot.Bot/Database/BotDatabase.cs
+++ SammBot.Bot/Database/BotDatabase.cs
@@ -12,6 +12,19 @@ namespace SammBot.Bot.Database
         public DbSet<UserWarning> UserWarnings { get; set; }
         public DbSet<GuildConfig> GuildConfigs { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<GuildConfig>().Property(x => x.WarningLimit).HasDefaultValue(3);
+            modelBuilder.Entity<GuildConfig>().Property(x => x.WarningLimitAction).HasDefaultValue(WarnLimitAction.None);
+            
+            modelBuilder.Entity<GuildConfig>().Property(x => x.EnableLogging).HasDefaultValue(false);
+            modelBuilder.Entity<GuildConfig>().Property(x => x.LogChannel).HasDefaultValue(0);
+            
+            modelBuilder.Entity<GuildConfig>().Property(x => x.EnableWelcome).HasDefaultValue(false);
+            modelBuilder.Entity<GuildConfig>().Property(x => x.WelcomeChannel).HasDefaultValue(0);
+            modelBuilder.Entity<GuildConfig>().Property(x => x.WelcomeMessage).HasDefaultValue("{0}, welcome to {1}! Remember to read the rules before chatting!");
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder OptionsBuilder)
         {
             string databaseFile = Path.Combine(Settings.Instance.BotDataDirectory, "bot.db");
--- SammBot.Bot/Database/DatabaseDefinitions/GuildConfig.cs
+++ SammBot.Bot/Database/DatabaseDefinitions/GuildConfig.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using SammBot.Bot.Classes;
 
 namespace SammBot.Bot.Database
 {
@@ -9,11 +10,38 @@ namespace SammBot.Bot.Database
         None
     }
 
+    // REMINDER: Set these default values in BotDatabase -> OnModelCreating too!
     public class GuildConfig
     {
         [Key]
         public ulong GuildId { get; set; }
-        public int WarningLimit { get; set; }
-        public WarnLimitAction WarningLimitAction { get; set; }
+
+        [FullName("Warning Limit")]
+        [FullDescription("The amount of warnings you can give to someone before they receive an automatic punishment.")]
+        public int WarningLimit { get; set; } = 3;
+        
+        [FullName("Warning Limit Action")]
+        [FullDescription("The automatic punishment to do when a user reaches the warning limit.\nValid values are `Kick`, `Ban` and `None`.")]
+        public WarnLimitAction WarningLimitAction { get; set; } = WarnLimitAction.None;
+
+        [FullName("Enable Logging")]
+        [FullDescription("Enable server event logging?")]
+        public bool EnableLogging { get; set; } = false;
+        
+        [FullName("Log Channel ID")]
+        [FullDescription("The channel ID where logs will be sent to. The bot must have permission to write to that channel.")]
+        public ulong LogChannel { get; set; }
+        
+        [FullName("Enable Welcome Message")]
+        [FullDescription("Enable welcome message?")]
+        public bool EnableWelcome { get; set; } = false;
+        
+        [FullName("Welcome Channel ID")]
+        [FullDescription("The channel ID where welcome messages will be sent to. The bot must have permission to write to that channel.")]
+        public ulong WelcomeChannel { get; set; }
+        
+        [FullName("Welcome Message Template")]
+        [FullDescription("The template for the welcome message.\n{0} is the user mention, {1} is the server name.")]
+        public string WelcomeMessage { get; set; } = "{0}, welcome to {1}! Remember to read the rules before chatting!";
     }
 }
