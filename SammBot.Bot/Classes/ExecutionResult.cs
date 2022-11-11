b4f54daf463f9eae8d818b301bf28b858c295ed5
AestheticalZ <51166756+AestheticalZ@users.noreply.github.com>
2022-11-11 14:00:58 +0100

    [Bot -> Classes] Adapt ExecutionResult to use interaction framework.

--- SammBot.Bot/Classes/ExecutionResult.cs
+++ SammBot.Bot/Classes/ExecutionResult.cs
@@ -1,13 +1,13 @@
-﻿using Discord.Commands;
+﻿using Discord.Interactions;
 
 namespace SammBot.Bot.Classes
 {
     public class ExecutionResult : RuntimeResult
     {
-        public ExecutionResult(CommandError? Error, string Reason) : base(Error, Reason) { }
+        public ExecutionResult(InteractionCommandError? Error, string Reason) : base(Error, Reason) { }
         
         public static ExecutionResult FromError(string Reason) =>
-            new ExecutionResult(CommandError.Unsuccessful, Reason);
+            new ExecutionResult(InteractionCommandError.Unsuccessful, Reason);
         
         public static ExecutionResult Succesful() =>
             new ExecutionResult(null, "Execution succesful.");
