99b756e33bb29d0ee934101dd1b82062f953eec7
AestheticalZ <51166756+AestheticalZ@users.noreply.github.com>
2022-11-11 14:33:54 +0100

    [Bot -> Core] Temporarily fix logger.

--- SammBot.Bot/Core/Loggers/Logger.cs
+++ SammBot.Bot/Core/Loggers/Logger.cs
@@ -1,10 +1,10 @@
 ﻿using Discord;
-using Discord.Commands;
 using Discord.WebSocket;
 using Matcha;
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using Discord.Interactions;
 
 namespace SammBot.Bot.Core
 {
@@ -12,7 +12,7 @@ namespace SammBot.Bot.Core
     {
         private readonly MatchaLogger _LoggerInstance;
 
-        public Logger(DiscordShardedClient Client, CommandService CommandService)
+        public Logger(DiscordShardedClient Client, InteractionService InteractionService)
         {
             //Default settings.
             MatchaLoggerSettings loggerSettings = new MatchaLoggerSettings()
@@ -26,7 +26,7 @@ namespace SammBot.Bot.Core
             _LoggerInstance = new MatchaLogger(loggerSettings);
 
             Client.Log += LogAsync;
-            CommandService.Log += LogAsync;
+            InteractionService.Log += LogAsync;
         }
 
         public void Log(string Message, LogSeverity Severity) =>
@@ -44,24 +44,24 @@ namespace SammBot.Bot.Core
                     Log(Message.Message, LogSeverity.Debug);
                     break;
                 case Discord.LogSeverity.Critical:
-                    if (Message.Exception is CommandException CriticalException)
-                    {
-                        string formattedException = string.Format("Critical exception in command \"{0}{1}\", at channel #{2}.\n{3}",
-                                Settings.Instance.LoadedConfig.BotPrefix, CriticalException.Command.Aliases[0], CriticalException.Context.Channel, CriticalException);
-
-                        Log(formattedException, LogSeverity.Fatal);
-                    }
-                    else Log(Message.Message, LogSeverity.Fatal);
+                    // if (Message.Exception is CommandException CriticalException)
+                    // {
+                    //     string formattedException = string.Format("Critical exception in command \"{0}{1}\", at channel #{2}.\n{3}",
+                    //             Settings.Instance.LoadedConfig.BotPrefix, CriticalException.Command.Aliases[0], CriticalException.Context.Channel, CriticalException);
+                    //
+                    //     Log(formattedException, LogSeverity.Fatal);
+                    // }
+                    /*else*/ Log(Message.Message, LogSeverity.Fatal);
                     break;
                 case Discord.LogSeverity.Error:
-                    if (Message.Exception is CommandException ErrorException)
-                    {
-                        string formattedException = string.Format("Exception in command \"{0}{1}\", at channel #{2}.\n{3}",
-                                Settings.Instance.LoadedConfig.BotPrefix, ErrorException.Command.Aliases[0], ErrorException.Context.Channel, ErrorException);
-
-                        Log(formattedException, LogSeverity.Error);
-                    }
-                    else Log(Message.Message, LogSeverity.Error);
+                    // if (Message.Exception is CommandException ErrorException)
+                    // {
+                    //     string formattedException = string.Format("Exception in command \"{0}{1}\", at channel #{2}.\n{3}",
+                    //             Settings.Instance.LoadedConfig.BotPrefix, ErrorException.Command.Aliases[0], ErrorException.Context.Channel, ErrorException);
+                    //
+                    //     Log(formattedException, LogSeverity.Error);
+                    // }
+                    /*else*/ Log(Message.Message, LogSeverity.Error);
                     break;
                 case Discord.LogSeverity.Warning:
                     Log(Message.Message, LogSeverity.Warning);
