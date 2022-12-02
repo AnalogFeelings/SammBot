ee7d731386634effecc505158a53204685bd931c
AestheticalZ <51166756+AestheticalZ@users.noreply.github.com>
2022-12-02 11:20:09 +0100

    [Bot -> Attributes] Add UglyName attribute.

---
+++ SammBot.Bot/Classes/Attributes/UglyName.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SammBot.Bot.Attributes;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class UglyName : Attribute
+{
+    public readonly string Name;
+
+    public UglyName(string Name) => this.Name = Name;
+}
