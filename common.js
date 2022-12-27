f1b26e73d67a6a5bf462337c4771ae3bb0671159
AestheticalZ <51166756+AestheticalZ@users.noreply.github.com>
2022-12-27 05:54:21 +0100

    Move navbar to the top.

--- common.js
+++ common.js
@@ -11,33 +11,7 @@ function LoadElements()
         $.get(file, function(html_content)
         {
             $(element).replaceWith(html_content);
-
-            if(name === "navbar") InitBanner();
         }, 'html');
-    }
-}
-
-function InitBanner()
-{
-    let bannerLogoElement = document.getElementById("bannerLogo");
-    let navBarElement = document.querySelector('[id="navBar"]');
-    let stickyBannerElement = document.querySelector('[id="stickyBanner"]');
-    let stickyObserver = new IntersectionObserver(([e]) => BannerCallback(e),
-        {
-            rootMargin: '-1px 0px 0px 0px',
-            threshold: [1]
-        });
-
-    stickyObserver.observe(stickyBannerElement);
-
-    function BannerCallback(e)
-    {
-        let isIntersecting = e.boundingClientRect.top === 0;
-
-        bannerLogoElement.classList.toggle("shown", isIntersecting);
-
-        stickyBannerElement.classList.toggle("sticky", isIntersecting);
-        navBarElement.classList.toggle("sticky", isIntersecting);
     }
 }
 
