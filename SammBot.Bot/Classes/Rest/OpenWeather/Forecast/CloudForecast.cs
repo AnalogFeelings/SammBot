991600f8e5c981cbcdf9b69815a108ed333c5fa0
AestheticalZ <51166756+AestheticalZ@users.noreply.github.com>
2023-01-01 16:24:22 +0100

    [All -> All] Happy new year. Update copyright headers.

--- SammBot.Bot/Classes/Rest/OpenWeather/Forecast/CloudForecast.cs
+++ SammBot.Bot/Classes/Rest/OpenWeather/Forecast/CloudForecast.cs
@@ -1,7 +1,7 @@
 ﻿#region License Information (GPLv3)
 /*
  * Samm-Bot - A lightweight Discord.NET bot for moderation and other purposes.
- * Copyright (C) 2021-2022 AestheticalZ
+ * Copyright (C) 2021-2023 AestheticalZ
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
