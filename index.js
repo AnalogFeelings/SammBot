513b76f2c1ddb14db1312d91cb121e77962ed8a3
AestheticalZ <51166756+AestheticalZ@users.noreply.github.com>
2023-01-22 21:24:31 +0100

    Remove twemoji as its unused as of now.

--- index.js
+++ index.js
@@ -4,10 +4,5 @@ function InitPage()
 {
     LoadElements();
 
-    twemoji.parse(document.body, {
-        folder: "svg",
-        ext: ".svg"
-    });
-
     window.addEventListener("mouseup", (e) => HandleClick(e));
 }
