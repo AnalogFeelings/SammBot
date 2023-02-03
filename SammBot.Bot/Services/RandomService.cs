a0cac8db1da3ad16ed983bf2ecdb9d06961caf9b
AestheticalZ <51166756+AestheticalZ@users.noreply.github.com>
2023-02-03 20:37:03 +0100

    [Bot -> Services] Remove random client.

--- SammBot.Bot/Services/RandomService.cs
+++ SammBot.Bot/Services/RandomService.cs
@@ -21,7 +21,6 @@
 using SammBot.Bot.Core;
 using SharpCat.Requester.Cat;
 using SharpCat.Requester.Dog;
-using System.Net.Http;
 
 namespace SammBot.Bot.Services;
 
@@ -30,12 +29,9 @@ public class RandomService
     public readonly SharpCatRequester CatRequester;
     public readonly SharpDogRequester DogRequester;
 
-    public readonly HttpClient RandomClient;
-
     public RandomService()
     {
         CatRequester = new SharpCatRequester(SettingsManager.Instance.LoadedConfig.CatKey);
         DogRequester = new SharpDogRequester(SettingsManager.Instance.LoadedConfig.DogKey);
-        RandomClient = new HttpClient();
     }
 }
