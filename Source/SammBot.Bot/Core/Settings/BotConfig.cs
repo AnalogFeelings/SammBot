07822a387e95feaebe4b2812dc4aca8fe26004af
AestheticalZ <51166756+AestheticalZ@users.noreply.github.com>
2023-02-21 11:52:43 +0100

    [Bot -> Attributes] Add SensitiveSetting attribute.

--- Source/SammBot.Bot/Core/Settings/BotConfig.cs
+++ Source/SammBot.Bot/Core/Settings/BotConfig.cs
@@ -49,11 +49,22 @@ public class BotConfig
     public bool WaitForDebugger { get; set; } = false;
     public string TwitchUrl { get; set; } = "https://www.twitch.tv/coreaesthetics";
     public string CommandLogFormat { get; set; } = "Executing command \"{0}\". Channel: #{1}. User: @{2}.";
-    public string HttpUserAgent { get; set; } = "Placeholder User Agent (.NET Application)";
+    [SensitiveSetting] public string HttpUserAgent { get; set; } = "Placeholder User Agent (.NET Application)";
 
     // API Tokens
-    [RequiresReboot] public string BotToken { get; set; } = string.Empty;
-    [RequiresReboot] public string CatKey { get; set; } = string.Empty;
-    [RequiresReboot] public string DogKey { get; set; } = string.Empty;
-    [RequiresReboot] public string OpenWeatherKey { get; set; } = string.Empty;
+    [RequiresReboot]
+    [SensitiveSetting]
+    public string BotToken { get; set; } = string.Empty;
+    
+    [RequiresReboot]
+    [SensitiveSetting]
+    public string CatKey { get; set; } = string.Empty;
+    
+    [RequiresReboot]
+    [SensitiveSetting]
+    public string DogKey { get; set; } = string.Empty;
+    
+    [RequiresReboot]
+    [SensitiveSetting]
+    public string OpenWeatherKey { get; set; } = string.Empty;
 }
